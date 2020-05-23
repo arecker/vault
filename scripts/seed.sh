2cc92ac57482c2b65f13c6a88e9dd16ccfbd1696
Alex Recker <alex@reckerfamily.com>
2020-05-23 12:35:15 -0500

    Seed

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -4,3 +4,14 @@ log() {
     echo "seed.sh $1" 1>&2
 }
 
+# cat <<EOF | kubectl apply -f -
+cat <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: test-secret
+data:
+  token: $(pass vault/token)
+  unseal: |
+$(pass vault/unseal | pr -t -o 4)
+EOF
