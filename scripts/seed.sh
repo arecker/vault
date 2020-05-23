25fde814c685601d835e0a21f382a762809f3c0a
Alex Recker <alex@reckerfamily.com>
2020-05-23 14:16:16 -0500

    Seed script

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -4,14 +4,24 @@ log() {
     echo "seed.sh $1" 1>&2
 }
 
-# cat <<EOF | kubectl apply -f -
-cat <<EOF
+aws() {
+    cat <<EOF
+[default]
+aws_access_key_id = $(pass vault/aws-access-key)
+aws_secret_access_key: $(pass vault/aws-secret-key)
+EOF
+}
+
+
+log "applying secrets from password store"
+cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: Secret
 metadata:
-  name: test-secret
+  name: vault
+  namespace: vault
 data:
-  token: $(pass vault/token)
-  unseal: |
-$(pass vault/unseal | pr -t -o 4)
+  root: $(pass vault/root | base64)
+  aws: $(aws | base64)
+  unseal: $(pass vault/unseal | base64)
 EOF
