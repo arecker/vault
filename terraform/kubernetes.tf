d0ca40e11aece8a4262c1b9f63748b22d1934559
Alex Recker <alex@reckerfamily.com>
2020-05-23 23:07:29 -0500

    Fix syntax

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -7,7 +7,7 @@ resource "vault_auth_backend" "kubernetes" {
 }
 
 resource "vault_kubernetes_auth_backend_config" "config" {
-  backend            = "${vault_auth_backend.kubernetes.path}"
+  backend            = vault_auth_backend.kubernetes.path
   kubernetes_host    = "https://farm-0:6443"
   kubernetes_ca_cert = file(local.cert_path)
 }
